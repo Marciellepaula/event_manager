64aa02df053473e02dfb920a9fd39f7d21a6a9c3
Marcielle Paula <marcielle0644@gmail.com>
2025-02-27 17:16:45 -0300

    ajustes test

--- tests/Unit/RegistrationControllerTest.php
+++ tests/Unit/RegistrationControllerTest.php
@@ -1,17 +1,16 @@
 <?php
 
-
 namespace Tests\Unit;
 
 use App\Models\Event;
 use App\Models\Registration;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Bus;
 use App\Jobs\SendEventSubscriptionEmail;
 use App\Jobs\SendEventUnsubscriptionEmail;
 use App\Services\EventSubscriptionService;
+use Illuminate\Support\Facades\Auth;
 use Mockery;
 use Tests\TestCase;
 
@@ -22,6 +21,7 @@ class RegistrationControllerTest extends TestCase
     use RefreshDatabase;
 
     private $eventSubscriptionService;
+
 
     protected function setUp(): void
     {
@@ -34,18 +34,18 @@ class RegistrationControllerTest extends TestCase
     public function it_subscribes_user_to_event_successfully()
     {
         Bus::fake();
-        $user = User::factory()->create();
-        $event = Event::factory()->create(['status' => 'open', 'max_capacity' => 100]);
 
-        Auth::login($user);
+        $user = User::factory()->make();
+        $event = Event::factory()->make(['status' => 'open', 'max_capacity' => 100]);
 
         $this->eventSubscriptionService
             ->shouldReceive('subscribeToEvent')
             ->once()
-            ->with($event->id)
+            ->with($user, $event->id)
             ->andReturn(['success' => 'Inscrição realizada com sucesso!']);
 
-        $response = app(EventSubscriptionService::class)->subscribeToEvent($user->id, $event->id);
+
+        $response = app(EventSubscriptionService::class)->subscribeToEvent($user, $event->id);
 
         $this->assertEquals(['success' => 'Inscrição realizada com sucesso!'], $response);
         Bus::assertDispatched(SendEventSubscriptionEmail::class);
@@ -54,19 +54,16 @@ class RegistrationControllerTest extends TestCase
     #[Test]
     public function it_shows_error_when_event_capacity_is_full()
     {
-        $user = User::factory()->create();
-        $event = Event::factory()->create(['status' => 'open', 'max_capacity' => 1]);
-        Registration::create(['user_id' => User::factory()->create()->id, 'event_id' => $event->id]);
-
-        Auth::login($user);
+        $user = User::factory()->make();
+        $event = Event::factory()->make(['status' => 'open', 'max_capacity' => 1]);
 
         $this->eventSubscriptionService
             ->shouldReceive('subscribeToEvent')
             ->once()
-            ->with($event->id)
+            ->with($user, $event->id)
             ->andReturn(['error' => 'Capacidade máxima atingida ou o evento não está mais aberto.']);
 
-        $response = app(EventSubscriptionService::class)->subscribeToEvent($user->id, $event->id);
+        $response = app(EventSubscriptionService::class)->subscribeToEvent($user, $event->id);
 
         $this->assertEquals(['error' => 'Capacidade máxima atingida ou o evento não está mais aberto.'], $response);
     }
@@ -74,18 +71,16 @@ class RegistrationControllerTest extends TestCase
     #[Test]
     public function it_shows_error_when_event_is_closed()
     {
-        $user = User::factory()->create();
-        $event = Event::factory()->create(['status' => 'closed', 'max_capacity' => 100]);
-
-        Auth::login($user);
+        $user = User::factory()->make();
+        $event = Event::factory()->make(['status' => 'closed', 'max_capacity' => 100]);
 
         $this->eventSubscriptionService
             ->shouldReceive('subscribeToEvent')
             ->once()
-            ->with($event->id)
+            ->with($user, $event->id)
             ->andReturn(['error' => 'Capacidade máxima atingida ou o evento não está mais aberto.']);
 
-        $response = app(EventSubscriptionService::class)->subscribeToEvent($user->id, $event->id);
+        $response = app(EventSubscriptionService::class)->subscribeToEvent($user, $event->id);
 
         $this->assertEquals(['error' => 'Capacidade máxima atingida ou o evento não está mais aberto.'], $response);
     }
@@ -93,63 +88,64 @@ class RegistrationControllerTest extends TestCase
     #[Test]
     public function it_shows_error_when_user_is_already_subscribed()
     {
-        $user = User::factory()->create();
-        $event = Event::factory()->create(['status' => 'open', 'max_capacity' => 100]);
-
-        Registration::create(['user_id' => $user->id, 'event_id' => $event->id]);
-
-        Auth::login($user);
+        $user = User::factory()->make();
+        $event = Event::factory()->make(['status' => 'open', 'max_capacity' => 100]);
 
         $this->eventSubscriptionService
             ->shouldReceive('subscribeToEvent')
             ->once()
-            ->with($event->id)
+            ->with($user, $event->id)
             ->andReturn(['error' => 'Você já está inscrito neste evento.']);
 
-        $response = app(EventSubscriptionService::class)->subscribeToEvent($user->id, $event->id);
+        $response = app(EventSubscriptionService::class)->subscribeToEvent($user, $event->id);
 
         $this->assertEquals(['error' => 'Você já está inscrito neste evento.'], $response);
     }
-
     #[Test]
     public function it_unsubscribes_user_from_event_successfully()
     {
         Bus::fake();
 
-        $user = User::factory()->create();
-        $event = Event::factory()->create(['status' => 'open', 'max_capacity' => 100]);
+        $user = User::factory()->make();
+        $event = Event::factory()->make(['status' => 'open', 'max_capacity' => 100]);
 
         Registration::create(['user_id' => $user->id, 'event_id' => $event->id]);
 
         Auth::login($user);
-
         $this->eventSubscriptionService
             ->shouldReceive('unsubscribeFromEvent')
             ->once()
-            ->with($event->id)
-            ->andReturn(['success' => 'Inscrição cancelada com sucesso!']);
+            ->with($user, $event->id)
+            ->andReturnUsing(function () use ($user, $event) {
 
-        $response = app(EventSubscriptionService::class)->unsubscribeFromEvent($user->id, $event->id);
+                SendEventUnsubscriptionEmail::dispatchSync($user->id, $event->id);
+                return ['success' => 'Inscrição cancelada com sucesso!'];
+            });
+
+        $response = $this->eventSubscriptionService->unsubscribeFromEvent($user, $event->id);
+
+        $this->assertEquals(['success' => 'Inscrição cancelada com sucesso!'], $response);
+
+        $response = app(EventSubscriptionService::class)->unsubscribeFromEvent($user, $event->id);
 
         $this->assertEquals(['success' => 'Inscrição cancelada com sucesso!'], $response);
         Bus::assertDispatched(SendEventUnsubscriptionEmail::class);
     }
 
+
     #[Test]
     public function it_shows_error_when_user_is_not_registered_for_event_to_unsubscribe()
     {
-        $user = User::factory()->create();
-        $event = Event::factory()->create(['status' => 'open', 'max_capacity' => 100]);
-
-        Auth::login($user);
+        $user = User::factory()->make();
+        $event = Event::factory()->make(['status' => 'open', 'max_capacity' => 100]);
 
         $this->eventSubscriptionService
             ->shouldReceive('unsubscribeFromEvent')
             ->once()
-            ->with($event->id)
+            ->with($user, $event->id)
             ->andReturn(['error' => 'Inscrição não encontrada.']);
 
-        $response = app(EventSubscriptionService::class)->unsubscribeFromEvent($user->id, $event->id);
+        $response = app(EventSubscriptionService::class)->unsubscribeFromEvent($user, $event->id);
 
         $this->assertEquals(['error' => 'Inscrição não encontrada.'], $response);
     }
