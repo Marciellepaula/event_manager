e2b2147e68c7581da38fbde5b2faea701ebfdacb
Marcielle Paula <marcielle0644@gmail.com>
2025-02-27 09:00:07 -0300

    criando view para emails

---
+++ resources/views/emails/event_subscribed.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Confirmação de Inscrição</title>
+</head>
+
+<body>
+    <h1>Olá, {{ $userName }}</h1>
+    <p>Você foi inscrito com sucesso no evento <strong>{{ $eventTitle }}</strong></p>
+    <p>Estamos ansiosos pela sua participação!</p>
+</body>
+
+</html>
---
+++ resources/views/emails/event_unsubscribed.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cancelamento de Inscrição</title>
+</head>
+
+<body>
+    <h1>Olá, {{ $userName }}</h1>
+    <p>Você cancelou sua inscrição no evento <strong>{{ $eventTitle }}</strong></p>
+    <p>Se isso foi um erro, entre em contato conosco para assistência.</p>
+</body>
+
+</html>
